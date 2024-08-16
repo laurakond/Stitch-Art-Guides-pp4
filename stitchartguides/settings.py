9fd254a5baac8f649d990a4c74be96a2ee10081d
laurakond <laura.kondrataite@gmail.com>
2024-08-16 12:45:17 +0100

    setup(create secret key)

--- stitchartguides/settings.py
+++ stitchartguides/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ubz@s=jeeiq*elt_swv1h6k81(4&-r(c*pt(*^57660yvszln='
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
