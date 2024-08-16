ca3fd6d25b82b45ceec72a170de129d31c6bcf77
laurakond <laura.kondrataite@gmail.com>
2024-08-16 17:18:37 +0100

    models(create tutorial model template)

--- tutorial/models.py
+++ tutorial/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Tutorial(models.Model):
+    """
+    A model that provides information about tutorial
+    """
+    title = models.CharField(max_length=150, unique=True, null=False, blank=False)
+    tutor_name = models.CharField(max_length=30, unique=True)
+    description = models.TextField(blank=True)
+    excerpt = models.TextField(blank=True)
+    date = models.TextField(blank=True)
+    time = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.title}"
