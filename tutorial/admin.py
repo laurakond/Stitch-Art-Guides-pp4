a3202e990d8a4549cfee3e43d68e2c26ea29ae6e
laurakond <laura.kondrataite@gmail.com>
2024-08-18 09:55:19 +0100

    models(register models in admin file)

--- tutorial/admin.py
+++ tutorial/admin.py
@@ -2,6 +2,34 @@ from django.contrib import admin
 from .models import Tutorial, TutorialDate, Booking
 
 # Register your models here.
-admin.site.register(Tutorial)
-admin.site.register(TutorialDate)
-admin.site.register(Booking)
+
+@admin.register(Tutorial)
+class TutorialAdmin(admin.ModelAdmin):
+    """
+    Populates filter fields in the database for
+    the Tutorial model.
+    """
+    list_display = ('title', 'tutor_name', 'description')
+    list_filter = ('title', 'tutor_name', 'description')
+    search_fields = ('title', 'tutor_name', 'description')
+
+@admin.register(TutorialDate)
+class TutorialDateAdmin(admin.ModelAdmin):
+    """
+    Populates filter fields in the database for
+    the TutorialDate model.
+    """
+    list_display = ('tutorial', 'tutorial_date')
+    list_filter = ('tutorial', 'tutorial_date')
+    search_fields = ('tutorial', 'tutorial_date')
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    """
+    Populates filter fields in the database for
+    the Booking model.
+    """
+    list_display = ('user', 'tutorial_date')
+    list_filter = ('user', 'tutorial_date')
+    search_fields = ('user', 'tutorial_date')
