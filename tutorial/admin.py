8272d33a119e20bec03cfc6ebb0e95bf7a5c1874
laurakond <laura.kondrataite@gmail.com>
2024-08-18 15:23:02 +0100

    views(set up initial views for tutorial app)
    setup(import summernote)

--- tutorial/admin.py
+++ tutorial/admin.py
@@ -1,27 +1,30 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Tutorial, TutorialDate, Booking
 
 # Register your models here.
 
 @admin.register(Tutorial)
-class TutorialAdmin(admin.ModelAdmin):
+class TutorialAdmin(SummernoteModelAdmin):
     """
     Populates filter fields in the database for
     the Tutorial model.
     """
-    list_display = ('title', 'tutor_name', 'description')
-    list_filter = ('title', 'tutor_name', 'description')
-    search_fields = ('title', 'tutor_name', 'description')
+    list_display = ('title', 'slug', 'tutor_name', 'excerpt')
+    list_filter = ('title', 'tutor_name')
+    search_fields = ('title', 'slug', 'tutor_name', 'description', 'excerpt')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('description',)
 
 @admin.register(TutorialDate)
-class TutorialDateAdmin(admin.ModelAdmin):
+class TutorialDateAdmin(SummernoteModelAdmin):
     """
     Populates filter fields in the database for
     the TutorialDate model.
     """
-    list_display = ('tutorial', 'tutorial_date')
-    list_filter = ('tutorial', 'tutorial_date')
-    search_fields = ('tutorial', 'tutorial_date')
+    list_display = ('tutorial_date', 'start_time', 'tutorial')
+    list_filter = ('tutorial_date', 'start_time', 'tutorial')
+    search_fields = ('tutorial_date', 'tutorial')
 
 
 @admin.register(Booking)
@@ -30,6 +33,6 @@ class BookingAdmin(admin.ModelAdmin):
     Populates filter fields in the database for
     the Booking model.
     """
-    list_display = ('user', 'tutorial_date')
-    list_filter = ('user', 'tutorial_date')
-    search_fields = ('user', 'tutorial_date')
+    list_display = ('tutorial_date', 'user')
+    list_filter = ('tutorial_date', 'user')
+    search_fields = ('tutorial_date', 'user')
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("", views.TutorialList.as_view(), name="tutorial_list"),
+    path("tutorials/", views.TutorialList.as_view(), name="tutorial_list"),
 ]
