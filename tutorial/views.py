2e7bddede019e301d4f635adc5d18dc98687e5e2
laurakond <laura.kondrataite@gmail.com>
2024-08-18 21:12:20 +0100

    views(set up index as home url path)

--- tutorial/views.py
+++ tutorial/views.py
@@ -3,7 +3,10 @@ from django.views import generic
 from .models import Tutorial, TutorialDate
 
 # Create your views here.
-
 class TutorialList(generic.ListView):
     queryset = Tutorial.objects.all()
-    template_name = "tutorial_list.html"
+    template_name = "tutorial/tutorial_list.html"
+
+
+def index(request):
+    return render(request, "tutorial/index.html")
