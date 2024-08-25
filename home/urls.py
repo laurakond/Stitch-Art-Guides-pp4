0798ed342db84ea2cacd191648652c9e01f15f15
laurakond <laura.kondrataite@gmail.com>
2024-08-25 18:43:44 +0100

    urls(add my tutorials url pattern)

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     
     path("", views.index, name="home"),
     path("about/", views.about_me, name="about"),
+    path("booked_tutorials/", views.test, name="booked_tutorials"),
 ]
