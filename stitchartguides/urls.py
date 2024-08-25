4abb90026a868832f1554f85c81c2efd3b930617
laurakond <laura.kondrataite@gmail.com>
2024-08-25 10:06:12 +0100

    urls(edit url pattern)

--- stitchartguides/urls.py
+++ stitchartguides/urls.py
@@ -19,9 +19,9 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('about/', include('home.urls')),
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path('summernote/', include('django_summernote.urls')),
-    path('', include("tutorial.urls"), name="tutorial-urls"),
+    path('tutorials/', include("tutorial.urls"), name="tutorial-urls"),
+    path('', include("home.urls"), name="home-urls"),
 ]
