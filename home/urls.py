78cd66b075c34e2774f3d2b41fe5d37ca7cbf37f
laurakond <laura.kondrataite@gmail.com>
2024-08-25 10:01:45 +0100

    index(relocate index file and adjust views/urls files)

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    
+    path("", views.index, name="home"),
     path("about/", views.about_me, name="about"),
 ]
--- home/views.py
+++ home/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 
 # Create your views here.
+def index(request):
+    """
+    function that displays home page.
+    """
+    return render(request, "home/index.html")
+
+
 def about_me(request):
+    """
+    function that displays about page.
+    """
     return render(request, "home/about.html")
