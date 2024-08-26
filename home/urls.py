4705d932739ee6d176d93b467a204909baad5d62
laurakond <laura.kondrataite@gmail.com>
2024-08-26 10:08:36 +0100

    urls(add url pattern for tutorial calendar)

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     
     path("", views.index, name="home"),
     path("about/", views.about_me, name="about"),
+    path("book_a_tutorial/", views.book_a_tutorial, name="calendar"),
     path("booked_tutorials/", views.my_tutorials, name="booked_tutorials"),
 ]
