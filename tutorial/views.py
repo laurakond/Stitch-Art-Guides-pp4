5632e0940ea96a9c40d7d22f155a68c45d6b0a69
laurakond <laura.kondrataite@gmail.com>
2024-08-26 10:07:07 +0100

    views(move calendar functionality to home directory)

--- tutorial/views.py
+++ tutorial/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Tutorial, TutorialDate
-import calendar
-from calendar import HTMLCalendar
-from datetime import datetime
 
 
 # Create your views here.
@@ -17,10 +14,7 @@ class TutorialList(generic.ListView):
 
 # the below code was appropriated from Code 
 # Institute's Blog walkthrough
-def tutorial_detail(request, 
-                    slug, 
-                    year=datetime.now().year, 
-                    month=datetime.now().strftime('%B')):
+def tutorial_detail(request, slug):
     """
     Function that displays individual tutorial's details.
     """
@@ -29,23 +23,11 @@ def tutorial_detail(request,
     tutorial = get_object_or_404(queryset, slug=slug)
     tutorial_date = TutorialDate.objects.all()
 
-    # the below code was appropriated from 
-    # codemy.com Youtube tutorial: 
-    # https://www.youtube.com/watch?v=4EJlrweJE-M&list=PLCC34OHNcOtqW9BJmgQPPzUpJ8hl49AGy&index=3
-    # convert month from name to number
-    month_number = list(calendar.month_name).index(month)
-    month_number = int(month_number)
-    
-    # create a calendar
-    tutorial_calendar = HTMLCalendar().formatmonth(year, month_number)
-
     return render(
         request,
         "tutorial/tutorial_detail.html",
         {"tutorial": tutorial,
          "coach": "Laura K",
          "tutorial_date": tutorial_date,
-         "tutorial_calendar": tutorial_calendar,
-         "month_number": month_number,
         },
     )
