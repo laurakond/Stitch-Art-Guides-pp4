617208220170d5c271f81266f1ac3868c952509e
laurakond <laura.kondrataite@gmail.com>
2024-08-26 10:08:03 +0100

    views(move calendar functionality from tutorial directory)

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from tutorial.models import Booking
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
 # Create your views here.
 def index(request):
@@ -29,3 +32,30 @@ def my_tutorials(request):
         'home/my_tutorials.html',
         {"bookings": bookings,}
          )
+
+
+# the below code was appropriated from Code 
+# Institute's Blog walkthrough
+def book_a_tutorial(request,  
+                    year=datetime.now().year, 
+                    month=datetime.now().strftime('%B')):
+    """
+    Function that displays calendar for booking a tutorial.
+    """
+    # the below code was appropriated from 
+    # codemy.com Youtube tutorial: 
+    # https://www.youtube.com/watch?v=4EJlrweJE-M&list=PLCC34OHNcOtqW9BJmgQPPzUpJ8hl49AGy&index=3
+    # convert month from name to number
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+    
+    # create a calendar
+    tutorial_calendar = HTMLCalendar().formatmonth(year, month_number)
+
+    return render(
+        request,
+        "home/book_tutorial.html",
+        {"tutorial_calendar": tutorial_calendar,
+         "month_number": month_number,
+        },
+    )
