a904e51694cbfd74fb5b2294747397ca6ec6ff0c
laurakond <laura.kondrataite@gmail.com>
2024-09-03 21:10:05 +0100

    views(create error pages views)

---
+++ stitchartguides/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+
+def error_400(request, exception):
+    """
+    Function that renders error 400 bad request. 
+    """
+    return render(request, "errors/400.html", status=400)
+
+
+def error_403(request, exception):
+    """
+    Function that renders error 403 access forbidden. 
+    """
+    return render(request, "errors/403.html", status=403)
+
+
+def error_404(request, exception):
+    """
+    Function that renders error 404 page not found. 
+    """
+    return render(request, "errors/404.html", status=404)
+
+
+def error_500(request):
+    """
+    Function that renders error 500 internal server error. 
+    """
+    return render(request, "errors/500.html", status=500)
