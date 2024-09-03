9d2b903ea38b9ef6208aa965943d1f6ea682263d
laurakond <laura.kondrataite@gmail.com>
2024-09-03 21:07:31 +0100

    settings(edit templates dirs)

--- stitchartguides/settings.py
+++ stitchartguides/settings.py
@@ -72,7 +72,10 @@ ROOT_URLCONF = 'stitchartguides.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [
+            os.path.join(BASE_DIR, "templates"),
+            os.path.join(BASE_DIR, "templates", "allauth"),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
