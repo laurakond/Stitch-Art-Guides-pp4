ec624f3d0889b01f91f7861c0673a7a4a8e21515
laurakond <laura.kondrataite@gmail.com>
2024-09-10 15:23:56 +0100

    urls(add url path for delete booking)

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("about/", views.about_me, name="about"),
     path("book-a-tutorial/", views.book_a_tutorial, name="calendar"),
-    path("booked-tutorials/", views.my_tutorials, name="booked_tutorials"),
     path("book-a-tutorial/<slug:slug>/<int:pk>/", views.tutorial_session, name="tutorial_slot"),
+    path("booked-tutorials/", views.my_tutorials, name="booked_tutorials"),
+    path("booked-tutorials/delete_booking/<int:booking_id>/", views.delete_booking, name="delete_booking"),
 ]
