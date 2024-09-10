ae43767211012c9d2a91cee19ad76d011eab2ef2
laurakond <laura.kondrataite@gmail.com>
2024-09-10 09:04:31 +0100

    js(move book button js code to separate file)

---
+++ static/js/book_button.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    /* Functionality for Booking modal */
+    function showBookingModal() {
+        let bookingEvent = new bootstrap.Modal(document.getElementById('bookingModal'));
+        bookingEvent.show();
+    }
+    // Event listener to trigger the modal when button is clicked
+    document.getElementById('bookButton').addEventListener('click', function () {
+        showBookingModal();
+    });
+});
