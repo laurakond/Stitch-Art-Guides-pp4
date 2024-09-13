a25b3a2a07e5ec1383c9316c68aa67c1f1fafefc
laurakond <laura.kondrataite@gmail.com>
2024-09-13 08:02:03 +0100

    urls(add edit_booking url path)

--- home/urls.py
+++ home/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("book-a-tutorial/", views.book_a_tutorial, name="calendar"),
     path("book-a-tutorial/<slug:slug>/<int:pk>/", views.tutorial_session, name="tutorial_slot"),
     path("booked-tutorials/", views.my_tutorials, name="booked_tutorials"),
+    path("booked-tutorials/edit_booking/<int:booking_id>/", views.edit_booking, name="edit_booking"),
     path("booked-tutorials/delete_booking/<int:booking_id>/", views.delete_booking, name="delete_booking"),
 ]
