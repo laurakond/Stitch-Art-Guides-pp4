f26f9ba2102580441291723e67fc0f98f975bf07
laurakond <laura.kondrataite@gmail.com>
2024-09-14 21:07:58 +0100

    urls(standardise function and view names)

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.TutorialList.as_view(), name="tutorial_list"),
-    path('dashboard/', views.dashboard_page, name='dashboard'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('dashboard/add_tutorial/', views.add_tutorial, name='add_tutorial'),
     path('dashboard/add_tutorial_date/', views.add_tutorial_date, name='add_tutorial_date'),
     path('<slug:slug>/', views.tutorial_detail, name='tutorial_detail'),
