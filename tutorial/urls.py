af7d06784465302b5e1c8c549a8d87ad7d188500
laurakond <laura.kondrataite@gmail.com>
2024-09-14 15:22:49 +0100

    urls(edit add tutorial path)

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,11 +1,11 @@
 from . import views
-from .views import AddTutorial, AddTutorialDate
+# from .views import AddTutorial, AddTutorialDate
 from django.urls import path
 
 
 urlpatterns = [
     path("", views.TutorialList.as_view(), name="tutorial_list"),
     path('dashboard/', views.dashboard_page, name='dashboard'),
-    path('dashboard/add_tutorial/', AddTutorial.as_view(), name='add_tutorial'),
+    path('dashboard/add_tutorial/', views.add_tutorial, name='add_tutorial'),
     path('<slug:slug>/', views.tutorial_detail, name='tutorial_detail'),
 ]
