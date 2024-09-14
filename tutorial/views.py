05eeaecf0e89630352384d3350514526ae7caadc
laurakond <laura.kondrataite@gmail.com>
2024-09-14 15:25:28 +0100

    views(edit add tutorial view)

--- tutorial/views.py
+++ tutorial/views.py
@@ -50,17 +50,44 @@ def dashboard_page(request):
         return redirect('account_login')
 
 
-class AddTutorial(CreateView):
-    """Add Tutorial view"""
+@login_required
+def add_tutorial(request):
+    """
+    Add tutorial view and generate add tutorial form.
+    """
 
-    template_name = "tutorial/add_tutorial.html"
-    model = Tutorial
-    form_class = TutorialForm
-    success_url = "/tutorial/"
+    tutorial_list = Tutorial.objects.all()
+    tutorial_form = TutorialForm()
 
-    def form_valid(self, form):
-        form.instance.user = self.request.superuser
-        return super(AddTutorial, self).form_valid(form)
+    # handling form post request. This part of the code was appropriated
+    # from I blog walkthrough project by Code Institute.
+    if request.user.is_superuser:
+        if request.method == "POST":
+            print("Received a POST request")
+            tutorial_form = TutorialForm(data=request.POST)
+            if tutorial_form.is_valid():
+                tutorial_form.author = request.user
+                tutorial_form.save()
+                messages.success(
+                    request,
+                    'New tutorial entry created. You can now add dates and times for it.'
+                    )
+                return redirect('dashboard')
+            else:
+                tutorial_form = TutorialForm()
+    else:
+        messages.error(request,
+                       'You do not have access to this content.'
+                       )
+        return redirect('account_login')
+
+    print("About to render template")
+    return render(request,
+                  "tutorial/add_tutorial.html",
+                  {"tutorial_list": tutorial_list,
+                   "tutorial_form":tutorial_form,
+                   },
+                  )
     
 
 class AddTutorialDate(CreateView):
