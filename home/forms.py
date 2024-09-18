9de0bd66e48057882cb1341ccc4d06d793c622bd
laurakond <laura.kondrataite@gmail.com>
2024-09-18 13:05:57 +0100

    forms(edit forms to exclude user booked tutorials)

--- home/forms.py
+++ home/forms.py
@@ -7,19 +7,24 @@ class BookingForm(forms.ModelForm):
     Form to create a Booking.
     """
 
-# Overriding init function to adjust filtering based on the date of the scheduled event.
-# code was taken and appropriated from 
-# https://stackoverflow.com/questions/43001425/django-modelform-with-conditions
-# https://forum.djangoproject.com/t/queryset-difference/15716
-# 
     def __init__(self, *args, **kwargs):
-        super(BookingForm, self).__init__(*args, **kwargs)
+        # grabbing the user from the arguments and storing in a separate parameter
+        self.user = kwargs.pop('user', None)
         current_date = datetime.now()
-        print(current_date)
-        self.fields['tutorial_date'].queryset = TutorialDate.objects.filter(
-            tutorial_date__gte=datetime.now()
-            )
 
+        # use reverse relationship to access the user from the views.py booking variable
+        # and assigning it to the user to remove from shown options
+        future_tutorials = TutorialDate.objects.filter(
+            tutorial_date__gte=current_date
+            ).exclude(booking__user=self.user)
+        
+        # Overriding init function to adjust filtering based on the date of the scheduled event.
+        # Code was appropriated from 
+        # https://stackoverflow.com/questions/43001425/django-modelform-with-conditions
+        # https://forum.djangoproject.com/t/queryset-difference/15716
+        super(BookingForm, self).__init__(*args, **kwargs)
+
+        self.fields['tutorial_date'].queryset = future_tutorials
 
     class Meta:
         model = Booking
