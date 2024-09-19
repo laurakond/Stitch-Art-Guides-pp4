e5b6adb7156850fe88937b02aba4023ef9ae83b7
laurakond <laura.kondrataite@gmail.com>
2024-09-19 19:13:00 +0100

    urls(add delete booking and tutorial date url paths)

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path("", views.TutorialList.as_view(), name="tutorial_list"),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('dashboard/add_tutorial/', views.add_tutorial, name='add_tutorial'),
+    path('dashboard/delete_booking/', views.delete_booking, name='delete_booking'),
     path('dashboard/add_tutorial_date/', views.add_tutorial_date, name='add_tutorial_date'),
     path('<slug:slug>/', views.tutorial_detail, name='tutorial_detail'),
+    path('dashboard/delete_tutorial_date/', views.delete_tutorial_date, name='delete_tutorial_date'),
 ]
