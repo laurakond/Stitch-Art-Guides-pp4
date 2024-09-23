4f683bcb47cbda379c125924cbde71d1c153782a
laurakond <laura.kondrataite@gmail.com>
2024-09-23 18:15:23 +0100

    urls(format and validate with CI PyLinter)

--- stitchartguides/urls.py
+++ stitchartguides/urls.py
@@ -1,30 +1,32 @@
-"""
-URL configuration for stitchartguides project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+"""stitch art guides project URL configuration"""
 from django.contrib import admin
 from django.urls import path, include
 from .views import error_400, error_403, error_404, error_500
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("accounts/", include("allauth.urls")),
-    path('summernote/', include('django_summernote.urls')),
-    path('tutorials/', include("tutorial.urls"), name="tutorial-urls"),
-    path('', include("home.urls"), name="home-urls"),
+    path(
+        'admin/',
+        admin.site.urls
+    ),
+    path(
+        "accounts/",
+        include("allauth.urls")
+    ),
+    path(
+        'summernote/',
+        include('django_summernote.urls')
+    ),
+    path(
+        'tutorials/',
+        include("tutorial.urls"),
+        name="tutorial-urls"
+    ),
+    path(
+        '',
+        include("home.urls"),
+        name="home-urls"
+    ),
 ]
 
 
