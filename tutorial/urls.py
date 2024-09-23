fede0cf34dd4c2f2cb60272686c9e6034a62a237
laurakond <laura.kondrataite@gmail.com>
2024-09-23 18:32:57 +0100

    urls(format and validate with CI PyLinter)

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -4,6 +4,14 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("", views.TutorialList.as_view(), name="tutorial_list"),
-    path('<slug:slug>/', views.tutorial_detail, name='tutorial_detail'),
+    path(
+        "",
+        views.TutorialList.as_view(),
+        name="tutorial_list"
+    ),
+    path(
+        '<slug:slug>/',
+        views.tutorial_detail,
+        name='tutorial_detail'
+    ),
 ]
