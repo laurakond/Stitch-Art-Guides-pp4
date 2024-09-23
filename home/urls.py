3f1bf80d1a9e60d122b99a6c90627b56ecafb732
laurakond <laura.kondrataite@gmail.com>
2024-09-23 18:06:01 +0100

    urls(format and validate with CI PyLinter)

--- home/urls.py
+++ home/urls.py
@@ -2,11 +2,39 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.index, name="home"),
-    path("about/", views.about_me, name="about"),
-    path("book-a-tutorial/", views.book_a_tutorial, name="book_a_tutorial"),
-    path("book-a-tutorial/<slug:slug>/<int:pk>/", views.tutorial_session, name="tutorial_session"),
-    path("my_tutorials/", views.my_tutorials, name="my_tutorials"),
-    path("my_tutorials/delete_booking/<int:booking_id>/", views.delete_booking, name="delete_booking"),
-    path("my_tutorials/edit_booking/<int:booking_id>/", views.edit_booking, name="edit_booking"),
+    path(
+        "",
+        views.index,
+        name="home"
+    ),
+    path(
+        "about/",
+        views.about_me,
+        name="about"
+        ),
+    path(
+        "book-a-tutorial/",
+        views.book_a_tutorial,
+        name="book_a_tutorial"
+    ),
+    path(
+        "book-a-tutorial/<slug:slug>/<int:pk>/",
+        views.tutorial_session,
+        name="tutorial_session"
+    ),
+    path(
+        "my_tutorials/",
+        views.my_tutorials,
+        name="my_tutorials"
+    ),
+    path(
+        "my_tutorials/delete_booking/<int:booking_id>/",
+        views.delete_booking,
+        name="delete_booking"
+    ),
+    path(
+        "my_tutorials/edit_booking/<int:booking_id>/",
+        views.edit_booking,
+        name="edit_booking"
+    ),
 ]
