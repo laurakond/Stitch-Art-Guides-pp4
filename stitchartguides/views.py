ac0bcb746602ec6ff44e9d6de7e36b4cf9cd4565
laurakond <laura.kondrataite@gmail.com>
2024-09-23 18:19:30 +0100

    views(format and validate with CI PyLinter)

--- stitchartguides/views.py
+++ stitchartguides/views.py
@@ -5,25 +5,41 @@ def error_400(request, exception):
     """
     Function that renders error 400 bad request. 
     """
-    return render(request, "errors/400.html", status=400)
+    return render(
+        request,
+        "errors/400.html",
+        status=400
+    )
 
 
 def error_403(request, exception):
     """
     Function that renders error 403 access forbidden. 
     """
-    return render(request, "errors/403.html", status=403)
+    return render(
+        request,
+        "errors/403.html",
+        status=403
+    )
 
 
 def error_404(request, exception):
     """
     Function that renders error 404 page not found. 
     """
-    return render(request, "errors/404.html", status=404)
+    return render(
+        request,
+        "errors/404.html",
+        status=404
+    )
 
 
 def error_500(request):
     """
     Function that renders error 500 internal server error. 
     """
-    return render(request, "errors/500.html", status=500)
+    return render(
+        request,
+        "errors/500.html",
+        status=500
+    )
