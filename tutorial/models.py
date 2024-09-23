6aaa57a9a8d6b446cf45ab515e3421ba991d4fa5
laurakond <laura.kondrataite@gmail.com>
2024-09-23 19:03:59 +0100

    models(add alt image field and format code)

--- tutorial/models.py
+++ tutorial/models.py
@@ -2,18 +2,40 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
 class Tutorial(models.Model):
     """
     A model that provides information about tutorial
     """
-    title = models.CharField(max_length=150, unique=True, null=False, blank=False)
-    slug = models.SlugField(max_length=150, unique=True)
-    tutor_name = models.CharField(max_length=30, unique=False)
-    description = models.TextField(blank=False)
-    excerpt = models.TextField(blank=False)
-    image = CloudinaryField('image', default='placeholder')
-
+    title = models.CharField(
+        max_length=150,
+        unique=True,
+        null=False,
+        blank=False
+    )
+    slug = models.SlugField(
+        max_length=150,
+        unique=True
+    )
+    tutor_name = models.CharField(
+        max_length=30,
+        unique=False
+    )
+    description = models.TextField(
+        blank=False
+    )
+    excerpt = models.TextField(
+        blank=False
+    )
+    image = CloudinaryField(
+        'image',
+        default='placeholder'
+    )
+    alt_text = models.CharField(
+        max_length=150,
+        unique=False,
+        verbose_name='alt text'
+    )
     def __str__(self):
         return f"{self.title}"
 
@@ -25,8 +47,11 @@ class TutorialDate(models.Model):
     tutorial_date = models.DateField()
     start_time = models.TimeField()
     end_time = models.TimeField()
-    tutorial = models.ForeignKey(Tutorial, on_delete=models.CASCADE, related_name="selected_tutorial_date")
-    #user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tutorial_user")
+    tutorial = models.ForeignKey(
+        Tutorial,
+        on_delete=models.CASCADE,
+        related_name="selected_tutorial_date"
+    )
 
     def __str__(self):
         return f"{self.tutorial_date} | {self.start_time} | {self.tutorial}"
