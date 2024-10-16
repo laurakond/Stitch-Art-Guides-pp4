9396c634b75a775e27982ffe8b4570c79307afab
laurakond <laura.kondrataite@gmail.com>
2024-10-16 08:02:10 +0100

    js(remove redundant code block)

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     /* Function that calls Book tutorial modal */
     function showBookingModal() {
-        let bookingEvent = new bootstrap.Modal(document.getElementById('bookingModal'));
+        let bookingEvent = new bootstrap.Modal(document.getElementById('booking-modal'));
         bookingEvent.show();
     }
     // Event listener to trigger the modal when button is clicked
@@ -11,22 +11,12 @@ document.addEventListener('DOMContentLoaded', function () {
             showBookingModal();
         });
     }
-    /* Function to render individual booking instance */
-    const editButtons = document.getElementsByClassName("editButton");
-    for (let button of editButtons) {
-        button.addEventListener("click", (e) => {
-            // Get associated booking ID
-            let bookingId = e.target.getAttribute("data-booking_id");
-            // Get the submit delete button elements
-            this.location.href = `edit_booking/${bookingId}`;
-        });
-    }
 
     /* Function to render individual delete booking instances for booked tutorials.
     * This part of the code was adjusted with the help of Tutor support from
     * Code Institute.
     */
-    const deleteButtons = document.getElementsByClassName("deleteButton");
+    const deleteButtons = document.getElementsByClassName("delete-button");
     for (let button of deleteButtons) {
         button.addEventListener("click", (e) => {
             // Get associated booking ID
