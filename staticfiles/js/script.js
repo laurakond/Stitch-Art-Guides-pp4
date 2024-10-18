6e294a93add04e8c63c7882bacf50ca04dfb88d1
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-10-18 11:48:42 +0000

    update staticfiles

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-    /* Booking modal for booking a tutorial functionality on book_a_tutorial.html */
+    /* Function that calls Book tutorial modal */
     function showBookingModal() {
-        let bookingEvent = new bootstrap.Modal(document.getElementById('bookingModal'));
+        let bookingEvent = new bootstrap.Modal(document.getElementById('booking-modal'));
         bookingEvent.show();
     }
     // Event listener to trigger the modal when button is clicked
@@ -13,40 +12,22 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    /* Code for Edit button functionality in booked_tutorials.html */
-    const editButtons = document.getElementsByClassName("editButton");
-
-    for (let button of editButtons) {
-        button.addEventListener("click", (e) => {
-            // Get associated booking ID
-            let bookingId = e.target.getAttribute("data-booking_id");
-            // Get the submit delete button elements
-            this.location.href = `edit_booking/${bookingId}`;
-        });
-    }
-
-    /* This part of the code was adjusted with the help of Tutor support from Code Institute.
-     Code for Delete booking Modal functionality in booked_tutorials.html. */
-    
-    // Get all delete buttons on booked_tutorials template
-    const deleteButtons = document.getElementsByClassName("deleteButton");
-
-    // Iterate through all delete buttons on template
+    /* Function to render individual delete booking instances for booked tutorials.
+    * This part of the code was adjusted with the help of Tutor support from
+    * Code Institute.
+    */
+    const deleteButtons = document.getElementsByClassName("delete-button");
     for (let button of deleteButtons) {
         button.addEventListener("click", (e) => {
             // Get associated booking ID
             let bookingId = e.target.getAttribute("data-booking_id");
             // Get the submit delete button elements
             const deleteConfirm = document.getElementById(`deleteConfirm-${bookingId}`);
-
             // Get associated Delete modal
             let deleteBookingEvent = new bootstrap.Modal(document.getElementById(`deletebookingModal-${bookingId}`));
             deleteConfirm.href = `delete_booking/${bookingId}`;
-
             // Show associated Delete modal
             deleteBookingEvent.show();
         });
     }
 });
-
-
